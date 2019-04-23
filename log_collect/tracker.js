da67324788f410741968d89d26c1b6f2e3fc875b
fuliming123 <46362904+fuliming123@users.noreply.github.com>
2019-04-23 18:24:25 +0800

    js

--- log_collect/tracker.js
+++ log_collect/tracker.js
@@ -51,8 +51,98 @@
             preVisitTime:"pre_visit_time"//用户最近一次访问时间
     },
         //定义需要采集的事件名称（这个意识是根据具体业务来
-        events:{
-            launchEvent:"e_"
+        events: {
+            launchEvent: "e_l",//用户首次访问事件
+            pageViewEvent: "e_pv",//用户浏览页面事件
+            addCartEvent: "e_ad",//商品加入购物车事件
+            searchEvent: "e_s"//搜索事件
+        },
+        /**
+         * 定义需要采集的字段（这个一定是根据具体业务来）
+         */
+        columns: {
+            eventName: "en",//事件名称
+            version: "ver",//日志版本
+            platform: "pl",//平台 ios Android
+            sdk: "sdk",//sdk js java
+            uuid: "uid",//用户唯一标识
+            sessionId: "sid",//会话id
+            resolution: "b_rst",//浏览器分辨率
+            userAgent: "b_usa",//浏览器代理信息
+            language: "l",//语言
+            clientTime: "ct",//客户端时间
+            currentUrl: "url",//当前页面的url
+            referrerUrl: "ref",//来源url，上一个页面的url
+            title: "tt",//网页标题
+            keyword: "kw",//搜索关键字
+            goodsId: "gid"//商品id
+        },
+        //设置uuid到cookie
+        setUuid:function (uuid) {
+            cookieUtils.set(this.cookieKeys.uuid,uuid)
+        },
+        //获取uuid
+        getUuid:function () {
+            return cookieUtils.get(this.cookieKeys.uuid)
+        },
+        //设置会话id
+        setSid:function (sid) {
+            cookieUtils.set(this.cookieKeys.sid,sid)
+        },
+        //获取会话id
+        getSid:function () {
+            return cookieUtils.get(this.cookieKeys.sid)
+        },
+        //todo 会话开始了
+        sessionStart :function () {
+            /**
+             * 判断会话是否存在，就是尝试从浏览器的cookie中获取用户的会话id，如果获取到了说明会话存在，否则不存在
+             * "" null ==>false
+             * */
+            if(!this.getSid()){
+                //会话不存在
+                this.createNewSession();
+
+            }else{
+                //会话存在
+            }
+        },
+        //创建新的会话
+        createNewSession:function () {
+            //生成会话id
+            var sid= guid();
+            //将会话id保存到cookie中
+            this.setSid(sid);
+
+            //判断用户是否是首次访问（查看浏览器的cookie中是否用用户的唯一标识，如果有说明是非首次访问，否则是首次访问）
+            if(!this.getUuid()){//获取不到用户的唯一标识
+                var uuis=guid();
+                //将用户唯一标识保持到cookie中
+                this.setUuid(uuid);
+                //todo 触发首次访问事件，收集用户的数据发送首次访问事件日志到后台服务器上
+            }
+        },
+        //定义首次访问事件
+        launchEvent:function () {
+            //定义一个对象data，这个对象用来封装手机号的数据
+            var data ={ };
+            //事件名称 data["en"]="e_l"
+            date[this.columns.eventName]=this.events.launchEvent;
+            //设置公共字段
+            this.setCommonColumns(data)
+
+        },
+        //设置公共字段
+        setCommonColums:function (data) {
+            //sdk 版本号
+            data[this.columns.version]=this.clientConfig.logVersion;
+        },
+        //生成唯一标识 guid
+        guid: function () {
+            return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
+                var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
+                return v.toString(16);
+            });
         }
 
     }
@@ -61,4 +151,4 @@
 
 
 
-})
+})();
